db7415a40a1d411944178e2e36c07e9cb7507d69
Daniel Kwon <danielkwon89@gmail.com>
2020-05-26 18:30:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,6 @@ class Song
   def self.new_by_filename(file_name)
     song_name = file_name.split(" - ")[1]
     song = Song.new(song_name)
-    song.artist = Artist.find_or_create_by_name
+    song.artist = Artist.find_or_create_by_name(song_name[0])
   end
 end
