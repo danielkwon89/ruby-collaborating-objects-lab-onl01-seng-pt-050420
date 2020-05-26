4ac460ba37b4da4332040fef666b5018235a51dd
Daniel Kwon <danielkwon89@gmail.com>
2020-05-26 18:11:11 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class MP3Importer
+  attr_accessor :path
+  
+  def initialize(file_path)
+    @path = file_path
+  end
+  
+  def files
+    files = Dir.children(@path)
+    binding.pry
+  end
+  
+  def import 
+  end
+end
