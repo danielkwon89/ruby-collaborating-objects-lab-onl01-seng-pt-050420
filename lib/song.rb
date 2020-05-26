a105281a92cef19ba56ecd58fa99f731a7de71ac
Daniel Kwon <danielkwon89@gmail.com>
2020-05-26 18:46:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,6 +23,7 @@ class Song
   end
   
   def artist_name=(artist_name)
-    Artist.find_or_create_by_name(artist_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    self.artist = artist
   end
 end
